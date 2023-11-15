0fa074afd21c4d1e05c205443b23581cc37e6696
Alp Serin <alpsrn@gmail.com>
2023-11-15 19:31:29 +0300

    added router

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const express = require("express");
 const app = express();
 
 const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
+
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
 const errorHandlerMiddleware = require("./middleware/error-handler");
@@ -16,6 +18,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
 });
+
+app.use("/api/v1/products", productsRouter); // every endpoint we add in the router will start with this endpoint
 
 // products route
 
